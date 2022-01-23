a43b2b95c96f1f4a6b19dc9fdc8faacf431ead40
chanapapan <chanapapan613@gmail.com>
2022-01-23 03:25:44 +0000

    rename encoder

--- Code/LSTM With Attention/main.py
+++ Code/LSTM With Attention/main.py
@@ -1,17 +1,17 @@
 import torch
-from model import Encoder, Decoder, Seq2Seq
+from model import Encoder
 from train import trainer
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Configured device: ", device)
 
-path = "../../../datasets/data_preprocessed_python"
+path = "../../../data_preprocessed_python"
 
 model_params = {
     "MODEL": "Hard-attention", # Self-attention, Soft-attention, Hard-attention, #Hierarchical-attention
     "STIM": "Arousal", #Arousal/ Valence/ All
     "BATCH_SIZE": 12,  # batch size
-    "EPOCHS": 1,  # number of training epochs
+    "EPOCHS": 2,  # number of training epochs
     "LEARNING_RATE": 0.001,  # learning rate
 }
 
@@ -25,6 +25,6 @@ output_dim = 1
 # dec = Decoder(256, 1).to(device)
 
 #connecting them with seq2seq and getting the final model out
-model = Seq2Seq(input_dim, hidden_dim, output_dim, model_params["MODEL"] ).to(device)
+model = Encoder(input_dim, hidden_dim, output_dim, model_params["MODEL"] ).to(device)
 
 trainer(path = path, model = model, model_params=model_params, device = device,)
--- Code/LSTM With Attention/model.py
+++ Code/LSTM With Attention/model.py
@@ -9,74 +9,56 @@ from attention_module import *
 class Encoder(nn.Module):
 
 #this class will initialize the models with the desired architecture
-    def __init__(self, input_size, embed_size,
-                 n_layers=1, dropout=0.5):
+    def __init__(self, input_dim, hidden_dim, output_dim, attention_type, n_layers=1, lstm_dropout=0.5): # decoder_dropout=0.2
         super(Encoder, self).__init__()
-        self.embed_size = embed_size
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
         
 # defining lstm and using bidirectional LSTM'S
-        self.lstm = nn.LSTM(input_size, embed_size, n_layers,
-                          dropout=dropout, bidirectional=True)
-# feed forward layer;s
-    def forward(self, x):       
-        output, (hn, cn) = self.lstm(x)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, n_layers,
+                          dropout=lstm_dropout, bidirectional=True)
         
-# sum bidirectional outputs
-        output = (output[:, :, :self.embed_size] +
-                   output[:, :, self.embed_size:])
-        return output, hn
-#encoder output is returned and passed to the decoder
-    
-#Decoder class 
-class Decoder(nn.Module):
-    def __init__(self, hidden_size, output_size, attention_type,
-                 dropout=0.2):
-        super(Decoder, self).__init__()
-
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        
         self.attention_type = attention_type
 #         self.dropout = nn.Dropout(dropout, inplace=True)
         
         ## attention layer
         if self.attention_type == "Self-attention":
-            self.attention = SelfAttention(hidden_size)
+            self.attention = SelfAttention(hidden_dim)
         elif self.attention_type == "Soft-attention":
-            self.attention = SoftAttention(hidden_size)
+            self.attention = SoftAttention(hidden_dim)
         elif self.attention_type == "Hard-attention":
-            self.attention = HardAttention(hidden_size)
+            self.attention = HardAttention(hidden_dim)
         elif self.attention_type == "Hierarchical-attention":
-            self.attention = HierarchicalAttention(hidden_size, output_size)    
+            self.attention = HierarchicalAttention(hidden_dim, output_dim)    
         else:
             raise ValueError('Undefined attention type')
-        self.fc = nn.Linear(hidden_size * 2, hidden_size)
-        self.out = nn.Linear(hidden_size * 2, output_size) 
+        self.fc = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.out = nn.Linear(hidden_dim * 2, output_dim) 
         self.sig = nn.Sigmoid()
-
-    def forward(self, last_hidden, encoder_outputs):
-
-# Calculate attention weights and apply to encoder outputs
+        
+# feed forward layer;s
+    def forward(self, x):       
+        output, (hn, cn) = self.lstm(x)
+        
+# sum bidirectional outputs
+        output = (output[:, :, :self.hidden_dim] +
+                   output[:, :, self.hidden_dim:])
+        
+        encoder_outputs = output
+        last_hidden = hn
+    
         attn_weights = self.attention(last_hidden[-1], encoder_outputs)
 #context vector=attention weights ,ecnoder outputs
 
 #[q*k]*v
         context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  
         context = context.transpose(0, 1)  
-        output = self.fc(last_hidden.view(-1, 2*self.hidden_size))
+        output = self.fc(last_hidden.view(-1, 2*self.hidden_dim))
         context = context.squeeze(0)
         output = self.out(torch.cat([output, context], 1))
 #output = F.log_softmax(output, dim=1)
-        return self.sig(output), attn_weights
 
-class Seq2Seq(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim,  attention_type):
-        super(Seq2Seq, self).__init__()
-        self.encoder = Encoder(input_dim, hidden_dim)
-        self.decoder = Decoder(hidden_dim, output_dim, attention_type)
+        self.attn_weights = attn_weights
 
-    def forward(self, src):
-
-        encoder_output, hidden = self.encoder(src) 
-        output, attn_weights = self.decoder(hidden, encoder_output)
-
-        return output
+        return self.sig(output)
